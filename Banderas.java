221bea4ff648c15624f831e2f2459da627fcaf3b
Mateo Agudelo Ciro <mateo.agudeloc@upb.edu.co>
2020-10-20 12:56:45 -0500

    v6

---
+++ Banderas.java
@@ -0,0 +1,45 @@
+public class Banderas{
+	public static void main(String[] args) {
+		
+		char matriz[][];
+		matriz = new char[18][27];
+			//i filas
+			//j columunas
+		 for (int i= 0; i<6; i++ ) {
+		 	for (int j=0; j<matriz[0].length; j++) {
+		 		matriz[i][j] = 'N';
+		 	}
+		 }
+		 
+		 for(int i=6; i<12; i++) {
+			 for(int j=0; j<matriz[0].length; j++){
+				 matriz[i][j] = 'R';
+			 }
+		 }
+
+		for(int i=12; i<18; i++){
+			for(int j=0; j<matriz[0].length; j++){
+				matriz[i][j] = 'A';
+			}
+		}
+
+
+		 for (int i=0;i<matriz.length ;i++ ) {
+		 	for (int j=0;j<matriz[0].length ;j++ ) {
+		 		if(matriz[i][j]=='N'){
+		 			System.out.print(ConsoleColors.BLACK_BACKGROUND+"   ");
+				 }
+				if(matriz[i][j]=='R'){
+					System.out.print(ConsoleColors.RED_BACKGROUND+"   ");
+				}
+				if(matriz[i][j] == 'A'){
+					System.out.print(ConsoleColors.YELLOW_BACKGROUND+"   ");
+				}
+		 		System.out.print(ConsoleColors.RESET);
+		 	}
+		 	System.out.println();
+		 }
+
+
+	}
+}
