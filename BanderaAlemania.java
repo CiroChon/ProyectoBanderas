5f7f73f35cf4beca34dcd9e888081372c0444782
Mateo Agudelo Ciro <mateo.agudeloc@upb.edu.co>
2020-10-20 11:32:05 -0500

    v3

--- BanderaAlemania.java
+++ BanderaAlemania.java
@@ -5,21 +5,33 @@ public class BanderaAlemania{
 		matriz = new char[18][27];
 			//i filas
 			//j columunas
-		 for (int i= 0; i<7 ;i++ ) {
+		 for (int i= 0; i<=6; i++ ) {
 
-		 	for (int j=0;j<matriz[0].length ;j++ ) {
+		 	for (int j=0; j<matriz[0].length; j++) {
 		 		matriz[i][j] = 'N';
 		 		
 		 	}
 		 }
 		 
+		 for(int i=7; i<=12; i++) {
+			 for(int j=0; j<matriz[0].length; j++){
+				 matriz[i][j] = 'R';
+			 }
+		 }
+
+
+
+
 		 for (int i=0;i<matriz.length ;i++ ) {
 
 		 	for (int j=0;j<matriz[0].length ;j++ ) {
 		 		if(matriz[i][j]=='N')
 		 		{
 		 			System.out.print(ConsoleColors.BLACK_BACKGROUND+"   ");
-		 		}
+				 }
+				if(matriz[i][j]=='R'){
+					System.out.print(ConsoleColors.RED_BACKGROUND+"    ");
+				}
 		 		System.out.print(ConsoleColors.RESET);
 		 	}
 		 	System.out.println();
