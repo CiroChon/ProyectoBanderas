ae2e893dc39be06d6896c349bcb571b1c8de79be
Mateo Agudelo Ciro <mateo.agudeloc@upb.edu.co>
2020-10-20 13:14:22 -0500

    v9

--- Banderas.java
+++ Banderas.java
@@ -1,7 +1,7 @@
 public class Banderas{
 	public static void main(String[] args) {
-		
 		String[] archivox = ConsoleFile.read("info_banderas.csv");
+		//Gran Bretaña
 		for(int n=1 ; n<21; n++){
 			String[] archivo = archivox[n].split(";");
 			for(int i = 0; i<archivo.length; i++){
@@ -22,7 +22,7 @@ public class Banderas{
 			}
 			System.out.println();
 		}
-
+		//Colombia
 		for(int n=21 ; n<41; n++){
 			String[] archivo = archivox[n].split(";");
 			for(int i = 0; i<archivo.length; i++){
@@ -42,25 +42,61 @@ public class Banderas{
 				System.out.print(ConsoleColors.RESET);
 			}
 		System.out.println();
-	}
-
-	for(int n=41 ; n<61; n++){
-		String[] archivo = archivox[n].split(";");
-		for(int i = 0; i<archivo.length; i++){
-			switch (archivo[i]) {
-				case "1":
-					System.out.print(ConsoleColors.RED_BACKGROUND+"   ");
-					break;
-				case "3":
-					System.out.print(ConsoleColors.WHITE_BACKGROUND+"   ");
-					break;
-				default:
-					System.out.print("   ");
-			}
-			System.out.print(ConsoleColors.RESET);
 		}
-	System.out.println();
-}
+		//Peru
+		for(int n=41 ; n<61; n++){
+			String[] archivo = archivox[n].split(";");
+			for(int i = 0; i<archivo.length; i++){
+				switch (archivo[i]) {
+					case "1":
+						System.out.print(ConsoleColors.RED_BACKGROUND+"   ");
+						break;
+					case "3":
+						System.out.print(ConsoleColors.WHITE_BACKGROUND+"   ");
+						break;
+					default:
+						System.out.print("   ");
+				}
+				System.out.print(ConsoleColors.RESET);
+			}
+		System.out.println();
+		}
+		//Polonia
+		for(int n=61 ; n<81; n++){
+			String[] archivo = archivox[n].split(";");
+			for(int i = 0; i<archivo.length; i++){
+				switch (archivo[i]) {
+					case "1":
+						System.out.print(ConsoleColors.RED_BACKGROUND+"   ");
+						break;
+					case "3":
+						System.out.print(ConsoleColors.WHITE_BACKGROUND+"   ");
+						break;
+					default:
+						System.out.print("   ");
+				}
+				System.out.print(ConsoleColors.RESET);
+			}
+		System.out.println();
+		}
+		//Indonesia
+		for(int n=81 ; n<101; n++){
+			String[] archivo = archivox[n].split(";");
+			for(int i = 0; i<archivo.length; i++){
+				switch (archivo[i]) {
+					case "1":
+						System.out.print(ConsoleColors.RED_BACKGROUND+"   ");
+						break;
+					case "3":
+						System.out.print(ConsoleColors.WHITE_BACKGROUND+"   ");
+						break;
+					default:
+						System.out.print("   ");
+				}
+				System.out.print(ConsoleColors.RESET);
+			}
+		System.out.println();
+		}
 
 
 
