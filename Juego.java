0f0acd6ba6e52f1b03d09531d2c0043af0340420
MaryJoP10 <maryjop10@gmail.com>
2020-10-21 17:26:39 -0500

    v20

--- Juego.java
+++ Juego.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class Juego {
     public static void main (String[] args){
-        String [] capitales = {"Londres", "Bogota", "Lima", "Varsovia", "Yakarta", "Roma", "Abuya", "Oslo", "Hanga Roa", "Bucarest", "Berlin", "Tokyo", "Mariehamn", "Seul", "Victoria", "Gibraltar", "Damasco", "Praga", "Beirut", "Edimburgo", "La Habana", "Moscu", "Sri Jayawardenapura Kotte", "Banjul", "Santiago De Chile", "Amsterdam", "Pionyang"};        
+        String [] capitales = {"Londres", "Bogota", "Lima", "Varsovia", "Yakarta", "Roma", "Abuya", "Oslo", "HangaRoa", "Bucarest", "Berlin", "Tokyo", "Mariehamn", "Seul", "Victoria", "Gibraltar", "Damasco", "Praga", "Beirut", "Edimburgo", "LaHabana", "Moscu", "SriJayawardenapuraKotte", "Banjul", "SantiagoDeChile", "Amsterdam", "Pionyang"};        
         int posAleatoria = (int) Math.floor(Math.random() * capitales.length); 
         String capital = capitales[posAleatoria];
 
@@ -14,12 +14,13 @@ public class Juego {
             guion[i]= '-';
         }
 
+        int intentos = 3;
+        
+        System.out.println("Cual es su capital?");
+        System.out.println(guion);
         boolean ganar = false;
-
         while(ganar == false) {
-            System.out.println("Cual es su capital?");
-            System.out.println(guion);
-
+ 
             Scanner lector = new Scanner(System.in);
             char letraIntro = lector.next().charAt(0);
             for (int i=0; i<letras.length; i++){
@@ -28,6 +29,7 @@ public class Juego {
                 } 
                 if (Arrays.equals (letras, guion)){
                     System.out.println("Esooo!");
+                    break;
                 }
             }
             System.out.println(guion);
