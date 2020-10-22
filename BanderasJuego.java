f3c713ccfa74152f1d8023616037b8ebb6576f67
MaryJoP10 <maryjop10@gmail.com>
2020-10-21 20:46:33 -0500

    v27

--- BanderasJuego.java
+++ BanderasJuego.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class BanderasJuego{
 	public static void main(String[] args) {
 		String[] archivox = ConsoleFile.read("info_banderas.csv");
@@ -633,5 +636,37 @@ public class BanderasJuego{
 		System.out.println();
         }
         }
+
+        boolean ganar = false;
+        while(ganar == false) {
+ 
+            Scanner lector = new Scanner(System.in);
+            char letraIntro = lector.next().charAt(0);
+            for (int i=0; i<letras.length; i++){
+                if(letras[i]==letraIntro){
+                    guion[i]=letraIntro;
+                } 
+                
+                if (Arrays.equals (letras, guion)){
+                    System.out.println("Esooo!");
+                    ganar = true;
+                    if(ganar == true) {
+                    break;
+                }
+                }
+            }
+            System.out.println(guion);
+        }
+
+
+
+
+
+
+
+
+
+
+
 	}
 }
